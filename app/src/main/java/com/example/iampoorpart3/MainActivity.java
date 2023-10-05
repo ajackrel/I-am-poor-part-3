bae4ada1c7acead5728be4ddce63bd042c634f3b
ajackrel <ajackrel@stratfordschools.com>
2023-10-05 13:55:46 -0700

    second commit - cleaned up instructions and code and comments

--- app/src/main/java/com/example/iampoorpart3/MainActivity.java
+++ app/src/main/java/com/example/iampoorpart3/MainActivity.java
@@ -8,13 +8,13 @@ ASSIGNMENT: I AM POOR PART 3
 ASSIGNMENT: I AM POOR PART 3
 -convert the component tree to CONSTRAINT LAYOUT (NOTE: Do not use Linear Layout for this assignment)
 -Rebuild the I am Poor app from part 2, but add the following:
--add a EditText XML component (NUMBER is probably the most appropriate)
-    -in the HINT attribute - enter 'How many gold bars do you own?' (without quotes)
--add event listener to the button
--when user touches the button (with a event listener) the:
-    -check the number the user enters - if more than 0, text changes from I AM RICH to I AM POOR
--add comments to every method and variable you have added. Yes, everything YOU ADDED
--add doc comment at top of .java with all necessary info.
+    -add a EditText XML component (NUMBER is probably the most appropriate)
+        -in the HINT attribute - enter 'How many gold bars do you own?' (without quotes)
+    -add an event listener to the button
+    -when user touches the button (with a event listener) the:
+        -check the number the user enters - if more than 0, text changes from I AM POOR to I AM RICH
+    -add comments to every method and variable you have added. Yes, everything YOU ADDED
+    -add doc comment at top of .java with all necessary info.
 -Commit and push to your repos to submit assignment
 
  */
@@ -48,11 +48,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //text.setTextColor(Color.GREEN);
+                //AS YOU CAN SEE - YOU HAVE TO CONVERT TO A STRING, THEN PARSE/CAST TO AN INT
+                //TextEdit CAN DISPLAY ONLY STRINGS (NO DOUBLES OR INT)
+                //***************************************************
+                //BE CAREFUL PROCEEDING!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //YOU MUST PARSE/CAST FOR THE APP TO WORK
+                //IF YOU GET CRASHES (IE, THE APP DISAPPEARS FROM THE SCREEN)
+                //CHECK YOUR CASTING!!!!
                 Integer.parseInt(number.getText().toString());
 
-
-                text.setText(number.getText());
+                text.setText(number.getText().toString());
 
             }
 
